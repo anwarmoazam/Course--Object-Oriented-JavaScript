8b467e6bb40c23ee65670e66b3b3ce27e6aeaff0
anwarmoazam <anwar.palimarwar@gmail.com>
2022-11-08 00:29:07 +0530

    practice of course

--- lesson_9.js
+++ lesson_9.js
@@ -1,10 +1,13 @@
 console.log('Lesson 9');
 
+let users = [];
+
 function User(userEmail,userName){
     this.email = userEmail;
     this.name = userName;
     this.online = false;
     this.score = 0;
+    users.push(this);
 }
 
 User.prototype.login = function(){
@@ -21,8 +24,22 @@ User.prototype.increseScore = function(){
     this.score++;
 }
 
+function Admin(...args){
+    User.apply(this,args);
+    this.role = 'super admin';
+}
+
+Admin.prototype = Object.create(User.prototype);
+
+Admin.prototype.deleteUser = function(u){
+    users = users.filter(user => {
+        return user.name != u.name;
+    });
+}
+
 let userOne = new User('anwar.moazam@hotmail.com','Anwar Moazam');
 let userTwo = new User('pali_idsp@yahoo.com','IDSP');
+let adminOne = new Admin('idsp.pali@gmail.com','Dr. Ankit');
 
 userOne.login();
 console.log(userOne);
@@ -30,3 +47,11 @@ userOne.increseScore();
 userOne.logout();
 console.log(userOne);
 console.log(userTwo);
+
+console.log(adminOne);
+adminOne.login()
+adminOne.logout()
+
+console.log(users)
+adminOne.deleteUser(userOne);
+console.log(users)
